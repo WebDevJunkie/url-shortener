79fd7308332abf79c7d29ec702558e0dde741b53
Mike Wittenauer <mikewittenauer@me.com>
2019-11-12 20:57:34 -0500

    Initial Commit

---
+++ UrlShortener/Controllers/UrlController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var urls = new List<object>()
+            {
+                new { Redirect = "http://www.google.com", Description = "Description" }
+            };
+
+            return Ok(urls);
+        }
+    }
+}
